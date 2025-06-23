6eefe6f9e3310932676b5a4629f204490ff7eee9
Fabio <fabio@MacBook-Air-M2-de-Fabio.local>
2025-06-23 15:14:54 -0300

    Resolvido conflito do merge

---
+++ Portifolio-Project-Fabio/src/App.tsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <h1>Fabio Daniel | Portfolio</h1>
+        <p>
+          Olá, sou o Fabio Daniel, um desenvolvedor web full stack.
+        </p>
+
+        <div className="links">
+          <a href="https://www.linkedin.com/in/fabiodanielataíde/" target="_blank">LinkedIn</a>
+          <a href="https://github.com/fabiodaniel123" target="_blank">GitHub</a>
+          </div>
+
+        <button>Conheça meu trabalho</button>
+
+      </div>
+    </>
+  )
+}
+
+export default App
